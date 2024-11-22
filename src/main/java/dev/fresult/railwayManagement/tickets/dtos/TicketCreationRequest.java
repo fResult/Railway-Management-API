22745c7101b7361090d6ccb501c5b673ef03a02c
fResult <styxmaz@gmail.com>
2024-11-22 18:29:39 +0700

    prepare mapper from TicketCreationRequest to the DAO entity

--- src/main/java/dev/fresult/railwayManagement/tickets/dtos/TicketCreationRequest.java
+++ src/main/java/dev/fresult/railwayManagement/tickets/dtos/TicketCreationRequest.java
@@ -1,9 +1,18 @@
 package dev.fresult.railwayManagement.tickets.dtos;
 
+import dev.fresult.railwayManagement.tickets.Ticket;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
 public record TicketCreationRequest(
-    @NotNull Integer trainTripId,
-    @NotNull Integer passengerId,
-    @NotBlank String seatNumber) {}
+    @NotNull Integer trainTripId, @NotNull Integer passengerId, @NotBlank String seatNumber) {
+
+  public static Ticket dtoToTicketCreate(TicketCreationRequest body) {
+    return new Ticket(
+        null,
+        AggregateReference.to(body.trainTripId()),
+        AggregateReference.to(body.passengerId()),
+        body.seatNumber());
+  }
+}
